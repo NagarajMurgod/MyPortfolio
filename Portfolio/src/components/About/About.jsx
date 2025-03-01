e5978b25163d90760172a837e5e811cc322cf0cf
NagarajMurgod <nagarajmurgod27oct@gmail.com>
2025-03-01 15:54:03 +0530

    my portfolio

---
+++ Portfolio/src/components/About/About.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+import experience from "../../data/experience.json";
+
+export const About = () => {
+  return (
+    
+    <>
+      <div className={styles.mainContainer}>
+        <h2 className={styles.title}>Experience</h2>
+        <section className={styles.container} id="about">
+          <div className={styles.content}>
+            <ul className={styles.aboutItems}>
+              {experience.map((exp,id) => {
+                return (
+                  <li key={id} className={styles.aboutItem}>
+                    <img src={getImageUrl(exp.company)} alt="Cursor icon" />
+                    <div className={styles.aboutItemText}>
+                      <h3>{exp.role}</h3>
+                      <h4>{exp.startEndDate}</h4>
+                      <ul className={styles.jobDescription}>
+                        {exp.experience.map((resp,id)=>{
+                          return (
+                            <li key={id}>
+                              {resp}
+                            </li>
+                        )})}
+                      </ul>
+                    </div>
+                  </li>
+                )})}
+            </ul>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+
+
+
+// Here are a few refined variations of your sentence:
+
+//     "I’m excited to fill this space with impactful contributions that drive the continued success of your team."
+//     "Looking forward to transforming this space into a showcase of my contributions to your team's ongoing success."
+//     "Eager to contribute meaningfully and fill this space with efforts that fuel your team's success."
+//     "Anticipating the opportunity to fill this space with contributions that propel your team's ongoing achievements."
+//     "I can’t wait to bring my skills and passion here, contributing to the lasting success of your team."
