6aebc756f082ba23b1830af1e3c7e93d76f01da3
NagarajMurgod <nagarajmurgod27oct@gmail.com>
2025-03-12 21:08:37 +0530

    test

--- Portfolio/src/components/Projects/Projects.jsx
+++ Portfolio/src/components/Projects/Projects.jsx
@@ -1,14 +1,31 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Projects.module.css";
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import projects from "../../data/projects.json";
+// import projects from "../../data/projects.json";
 import { ProjectCard } from "./ProjectCard";
+import axios from "axios";
+
 
 export const Projects = () => {
 
   const Scrollref = useRef(null);
+  const [projects, setProjects] = useState([]);
 
+
+  const getProjects = async () => {
+
+    try{
+      const resp = await axios.get("https://raw.githubusercontent.com/NagarajMurgod/MyPortfolio/refs/heads/main/Portfolio/src/data/projects.json");
+      setProjects(resp.data);
+    }catch(error){
+        console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getProjects();
+  },[])
 
   const RightScroll = () =>{
     let element =  Scrollref.current
@@ -33,7 +50,7 @@ export const Projects = () => {
         <KeyboardArrowLeftIcon onClick={LeftScroll} className={styles.arrows}/>
         <KeyboardArrowRightIcon onClick={RightScroll} className={styles.arrows}/>
         {projects.map((project, id) => {
-          return <ProjectCard key={id} project={project} />;
+          return <ProjectCard key={id} project={project} className={styles.projectCard} />;
         })}
       </div>
     </section>
